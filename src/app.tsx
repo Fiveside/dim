1c6043a79c206fc2960b7fdf50cadc5846a87f19
Erich Healy <ErichRHealy@gmail.com>
2019-09-22 20:32:33 -0700

    Add react and typescript

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>hell world from react!</div>,
+  document.getElementById('root')
+);
--- webpack.rules.js
+++ webpack.rules.js
@@ -6,7 +6,9 @@ module.exports = [
   },
   {
     test: /\.(m?js|node)$/,
-    parser: { amd: false },
+    parser: {
+      amd: false
+    },
     use: {
       loader: '@marshallofsound/webpack-asset-relocator-loader',
       options: {
@@ -16,18 +18,14 @@ module.exports = [
   },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
-  /**
-   * Typescript Example:
-   *
-   * {
-   *   test: /\.tsx?$/,
-   *   exclude: /(node_modules|.webpack)/,
-   *   loaders: [{
-   *     loader: 'ts-loader',
-   *     options: {
-   *       transpileOnly: true
-   *     }
-   *   }]
-   * }
-   */
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|.webpack)/,
+    loaders: [{
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }]
+  }
 ];
